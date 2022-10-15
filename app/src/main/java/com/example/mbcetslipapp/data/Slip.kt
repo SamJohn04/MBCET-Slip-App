ce0b668e457ba431aaf5a0f65144cf1cb3eb9d4f
Samuel John <samjohnmalayam@gmail.com>
2022-10-15 23:02:31 +0530

    Boopty boop?

--- app/src/main/java/com/example/mbcetslipapp/data/Slip.kt
+++ app/src/main/java/com/example/mbcetslipapp/data/Slip.kt
@@ -1,11 +1,11 @@
-package com.example.slipprojecttrialerror.data
+package com.example.mbcetslipapp.data
 
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.res.stringResource
-import com.example.slipprojecttrialerror.R
+import com.example.mbcetslipapp.R
 
 data class Slip(
     val name: String = "",
@@ -28,8 +28,8 @@ data class Slip(
 val slips = mutableListOf(
     Slip(name = "Samuel John", slipType = R.string.slip_misc, title = "Trials", slipDescribe = "Trial And Error", className = "CS 2", semester = 3, advisors = listOf("Ms. Gayathri K. S.","Mr. Praveen J S","Ms. Sreeja S R"), HoD = R.string.hod_cs, rollNo = "B21CS1234"),
     Slip(name = "Ab Cde", slipType = R.string.slip_id, title = "Request for new ID", slipDescribe = "This is to request for a new id. Unfortunately, my ID is lost, and I would like to request a replacement.", semester = 7, className = "CS 1", advisors = listOf("Dun", "Dun Dun", "Duuun"), HoD = R.string.hod_cs, rollNo = "B19CS1111"),
-    Slip(name = "Calma Von Feincherstire", slipType = R.string.slip_misc, title = "Trials", slipDescribe = "Trial And Error", semester = 5, className = "CS 1", advisors = listOf("Tana", "Nana"), HoD = R.string.hod_cs),
-    Slip(name = "Jacob", slipType = R.string.slip_misc, title = "Trials", slipDescribe = "Trial And Error", semester = 1, className = "CS 2", HoD = R.string.hod_cs),
-    Slip(name = "Frieren", slipType = R.string.slip_gate, title = "Permission to Leave Early", slipDescribe = "I am feeling fever and extreme headaches.\n Please excuse me for the day.", semester = 1, className = "CS 2", advisors = listOf("Never", "Gonna"), HoD = R.string.hod_cs),
-    Slip(name = "Samuel John", slipType = R.string.slip_misc, title = "Trials", slipDescribe = "Trial And Error",  semester = 3, className = "CS 21", HoD = R.string.hod_cs)
+    Slip(name = "Calma Von Feincherstire", slipType = R.string.slip_misc, title = "Trials", slipDescribe = "Trial And Error", semester = 5, className = "CS 1", advisors = listOf("Tana", "Nana"), HoD = R.string.hod_cs, rollNo = "B20CS1100"),
+    Slip(name = "Jacob", slipType = R.string.slip_misc, title = "Trials", slipDescribe = "Trial And Error", semester = 1, className = "CS 2", HoD = R.string.hod_cs, rollNo = "B22CS1232"),
+    Slip(name = "Frieren", slipType = R.string.slip_gate, title = "Permission to Leave Early", slipDescribe = "I am feeling fever and extreme headaches.\n Please excuse me for the day.", semester = 1, className = "CS 2", advisors = listOf("Never", "Gonna"), HoD = R.string.hod_cs, rollNo = "B22CS1247"),
+    Slip(name = "Samuel John", slipType = R.string.slip_misc, title = "Trials", slipDescribe = "Trial And Error",  semester = 3, className = "CS 21", HoD = R.string.hod_cs, rollNo = "B21CS1212")
 )
--- app/src/main/java/com/example/mbcetslipapp/ui/theme/SlipViewModel.kt
+++ app/src/main/java/com/example/mbcetslipapp/ui/theme/SlipViewModel.kt
@@ -1,6 +1,7 @@
-package com.example.slipprojecttrialerror.ui.theme
+package com.example.mbcetslipapp.ui.theme
 
 import androidx.lifecycle.ViewModel
+import com.example.mbcetslipapp.ui.theme.SlipUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,16 +13,25 @@ class SlipViewModel: ViewModel() {
         resetGame()
     }
     fun resetGame() {
-        _uiState.value = SlipUiState(userType = "Student", selection = "Requested")
+        _uiState.value = SlipUiState(userType = "Student", selection = "Requested", name = "Samuel John", studentRoll = "B21CS1212")
     }
     fun updateUserType(type: String)
     {
         val selection = _uiState.value.selection
-        _uiState.value = SlipUiState(type,selection)
+        val name = _uiState.value.name
+        val studentRoll = _uiState.value.studentRoll
+        _uiState.value = SlipUiState(type, selection, name, studentRoll)
     }
     fun updateSelection(type: String)
     {
         val user = _uiState.value.userType
-        _uiState.value = SlipUiState(user,type)
+        val name = _uiState.value.name
+        val studentRoll = _uiState.value.studentRoll
+        _uiState.value = SlipUiState(user, type, name, studentRoll)
+    }
+    fun updateUser(userType: String, name: String, studentRoll: String)
+    {
+        val selection = _uiState.value.selection
+        _uiState.value = SlipUiState(userType, selection, name, studentRoll)
     }
 }
